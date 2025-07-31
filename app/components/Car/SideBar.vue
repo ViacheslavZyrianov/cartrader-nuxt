843626e7de33e6f333b49f2168b1452f9feb2b85
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-07-31 23:39:03 +0300

    add error handling and modal functionality in car and sidebar components

--- app/components/Car/SideBar.vue
+++ app/components/Car/SideBar.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
+const route = useRoute()
 
+const modal = ref({
+  location: false,
+  make: false,
+  price: false,
+})
+
+const modalCity: Ref<string> = ref("")
+
+const onUpdateModal = (key: string) => {
+  modal.value[key] = !modal.value[key]
+}
+
+const onSubmitChangeLocation = () => {
+  if (!modalCity.value) return
+  if (!isNaN(parseInt(modalCity.value))) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Invalid city name',
+      message: 'City name cannot be a number'
+    })
+  }
+  onUpdateModal('location')
+  navigateTo(`/city/${modalCity.value}/car/${route.params.make}`)
+  modalCity.value = ""
+}
 </script>
 
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b" >
       <h3>Location</h3>
-      <h3 class="text-blue-400 capitalize">Kyiv</h3>
-      <!--          <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">-->
-      <!--            <input type="text" class="border p-1 rounded" placeholder="Search location">-->
-      <!--            <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>-->
-      <!--          </div>-->
+      <h3 class="text-blue-400 capitalize" @click="onUpdateModal('location')">
+        {{ route.params.city }}
+      </h3>
+      <form v-if="modal.location" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" @submit.prevent="onSubmitChangeLocation">
+        <input v-model="modalCity" type="text" class="border p-1 rounded" placeholder="Search location">
+        <button type="submit" class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+      </form>
     </div>
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b" >
--- app/pages/city/[city]/car.vue
+++ app/pages/city/[city]/car.vue
@@ -14,12 +14,25 @@ useHead({
 definePageMeta({
   layout: 'custom',
 })
+const onGoBack = (error) => {
+  error.value = null
+}
 </script>
 
 <template>
   <div class="mt-32 flex">
-    <CarSideBar />
-    <NuxtPage />
+    <NuxtErrorBoundary>
+      <CarSideBar />
+      <NuxtPage />
+
+      <template #error="{ error }">
+        <div class="text-center mx-auto flex flex-col">
+          <h1 class="text-5xl">Sorry, something went wrong</h1>
+          <h3>{{ error }}</h3>
+          <button class="rounded-full mt-7 text-2xl text-blue-500 bg-sky-100 hover:bg-sky-200 transition-colors px-6 py-2" @click="onGoBack">Go back</button>
+        </div>
+      </template>
+    </NuxtErrorBoundary>
   </div>
 </template>
 
