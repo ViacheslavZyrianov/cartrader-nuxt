5f57378017953ea36bf52620314d190bf7a68a7f
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-07-31 18:03:25 +0300

    initialize Nuxt.js project with basic configuration and structure

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<script setup lang="js">
+</script>
+
+<template>
+  <header class="sticky top-0 z-50 flex items-center justify-between bg-white p-4 shadow-md">
+    <a class="text-3xl font-mono" href="">Cartrader</a>
+  </header>
+</template>
