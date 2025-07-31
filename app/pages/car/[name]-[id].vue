9652a61632f24be4da48669a624b891ad7a48124
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-07-31 22:20:09 +0300

    add custom layout and update car detail components

--- app/pages/car/[name]-[id].vue
+++ app/pages/car/[name]-[id].vue
@@ -6,10 +6,14 @@ const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.sl
 useHead({
   title: capitalize(route.params.name)
 })
+
+definePageMeta({
+  layout: 'custom',
+})
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+  <div>
     <CarDetailHero />
     <CarDetailAttributes />
     <CarDetailDescription />
