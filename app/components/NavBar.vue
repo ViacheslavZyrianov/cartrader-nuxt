cfcd513c61822d257ed4813ada2e2dea0baad6ab
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-01 14:44:04 +0300

    add logout functionality and user profile link to NavBar component

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+const router = useRouter()
 
+const onLogout = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) {
+    console.error('Logout failed:', error)
+  } else {
+    router.push('/login')
+  }
+}
 </script>
 
 <template>
   <header class="sticky top-0 z-50 flex items-center justify-between bg-white p-4 shadow-md">
     <NuxtLink class="text-3xl font-mono" to="/">Cartrader</NuxtLink>
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer" @click="onLogout">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
 </template>
 
