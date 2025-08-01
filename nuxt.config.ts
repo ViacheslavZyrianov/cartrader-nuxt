0e32175bfd3a7d99bde6c59a98364f5645189bdf
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-01 14:11:14 +0300

    add Supabase redirect configuration to Nuxt settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image', '@vueuse/nuxt', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  }
 })
