a6b4cdbf9bedbd55b215d4e0c6f141d9174d34e6
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-05 18:25:15 +0300

    add form submission logic and disable button state in create.vue

--- app/pages/profile/listings/create.vue
+++ app/pages/profile/listings/create.vue
@@ -1,9 +1,10 @@
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   layout: "custom",
 });
 
 const { makes } = useCars();
+const user = useSupabaseUser()
 
 const info = useState("adInfo", () => ({
   make: "",
@@ -46,7 +47,7 @@ const inputs = [
   {
     id: 4,
     title: "Price *",
-    name: "Price",
+    name: "price",
     placeholder: "$4000",
   },
   {
@@ -68,6 +69,43 @@ const inputs = [
     placeholder: "Leather Interior, No Accidents",
   },
 ];
+
+const isButtonSubmitDisabled: ComputedRef<boolean> = computed(() => Object.values(info.value).some(value => value === "" || value === null));
+
+const buttonSubmitClassList = computed(() => ({
+  'text-white rounded py-2 px-8 mr-auto mt-4': true,
+  'bg-blue-400': !isButtonSubmitDisabled.value,
+  'bg-gray-400 cursor-not-allowed': isButtonSubmitDisabled.value,
+}));
+
+const onFormSubmit = async () => {
+  const body = {
+    ...info.value,
+    name: `${info.value.make} ${info.value.model}`,
+    city: info.value.city,
+    description: info.value.description,
+    features: info.value.features.split(", "),
+    numberOfSeats: parseInt(info.value.seats, 10),
+    year: parseInt(info.value.year, 10),
+    price: parseInt(info.value.price, 10),
+    miles: parseInt(info.value.miles, 10),
+    image: info.value.image ? info.value.image.name : null,
+    listerId: user.value.id
+  };
+
+  delete body.seats;
+
+  try {
+    await $fetch("/api/car/listings", {
+      method: "POST",
+      body,
+    });
+
+    navigateTo('/profile/listings');
+  } catch (error) {
+    errorMessage.value = error.statusMessage;
+  }
+}
 </script>
 
 
@@ -76,7 +114,8 @@ const inputs = [
     <div class="mt-24">
       <h1 class="text-6xl">Create a New Listing</h1>
     </div>
-    <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+    <pre>{{info}}</pre>
+    <form class="shadow rounded p-3 mt-5 flex flex-wrap justify-between" @submit.prevent="onFormSubmit">
       <CarAdSelect
         title="Make *"
         :options="makes"
@@ -98,7 +137,19 @@ const inputs = [
         @change-input="onChangeInput"
       />
       <CarAdImage @change-input="onChangeInput" />
-      <button type="submit" class="bg-blue-400 text-white rounded py-2 px-8 mr-auto mt-4">Submit</button>
-    </div>
+      <div>
+        <button
+          type="submit"
+          :disabled="isButtonSubmitDisabled"
+          :class="buttonSubmitClassList"
+        >
+          Submit
+        </button>
+
+        <p v-if="errorMessage" class="text-red-400 mt-2">
+          {{ errorMessage }}
+        </p>
+      </div>
+    </form>
   </div>
 </template>
