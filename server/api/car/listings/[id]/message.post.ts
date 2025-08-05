527f9c2ecac222c389c834e246e88d8fd12a75e8
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-05 14:45:36 +0300

    add message.post.ts endpoint to create messages for a listing

---
+++ server/api/car/listings/[id]/message.post.ts
@@ -0,0 +1,32 @@
+import Joi from 'joi';
+import { PrismaClient } from '@/generated/prisma';
+
+const schema = Joi.object({
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(10).pattern(/^[0-9]+$/),
+    name: Joi.string().required(),
+    message: Joi.string().min(20).required(),
+})
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    const { id } = event.context.params;
+
+    const { error } = schema.validate(body);
+
+    if (error) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: error.message,
+        });
+    }
+
+    return prisma.message.create({
+        data: {
+            ...body,
+            listingId: Number(id)
+        }
+    })
+})
