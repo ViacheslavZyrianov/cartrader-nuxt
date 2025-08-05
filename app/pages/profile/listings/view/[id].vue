2cb80bf07ff1817058c9b4b0dfaf54c703e9f0b3
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-05 20:30:25 +0300

    refactor message display in [id].vue and MessageCard.vue to use dynamic data

--- app/pages/profile/listings/view/[id].vue
+++ app/pages/profile/listings/view/[id].vue
@@ -2,13 +2,14 @@
 definePageMeta({
   layout: "custom",
 });
+
+const route = useRoute();
+
+const { data: messages } = await useFetch(`/api/car/listings/${route.params.id}/message`)
 </script>
 
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    <CarMessageCard v-for="message in messages" :key="message.id" :message="message" />
   </div>
 </template>
