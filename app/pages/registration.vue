7c87cacfe015a163365dc6840eba411dc5a8086e
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-06 23:05:19 +0300

    add registration component with form handling for user sign-up

---
+++ app/pages/registration.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+useHead({
+  title: 'Registration',
+})
+
+definePageMeta({
+  layout: 'custom'
+})
+
+const { useSignUp } = useAuth()
+
+const form = reactive({
+  email: '',
+  password: ''
+})
+
+const onSubmit = async () => {
+  await useSignUp(form)
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-6xl my-8 font-semibold">Registration</h1>
+    <form @submit.prevent="onSubmit">
+      <label for="email">Email</label>
+      <input id="email" v-model="form.email" type="email" name="email" placeholder="Enter your email" class="border p-2 mb-4 w-full">
+
+      <label for="password">Password</label>
+      <input id="password" v-model="form.password" type="password" name="password" placeholder="Enter password" class="border p-2 mb-4 w-full">
+
+      <button type="submit" class="bg-blue-400 mt-2 rounded text-white py-1 px-4">
+        Register
+      </button>
+    </form>
+  </div>
+</template>
