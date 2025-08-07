9e8aee3124e33c8e2aa35a7e44de3c3cd9dedbaf
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-07 15:26:13 +0300

    add check-your-email component for account verification instructions

---
+++ app/pages/check-your-email.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'custom',
+})
+</script>
+
+<template>
+  <div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">Check Your Email</h1>
+    <p class="text-lg">We have sent you an email with a link to verify your account. Please check your inbox and follow the instructions.</p>
+    <p class="text-lg mt-4">If you don't see the email, please check your spam folder.</p>
+  </div>
+</template>
