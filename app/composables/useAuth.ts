c478441701f7595b9b2160b822e20542ef773fb1
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-07 15:26:27 +0300

    feat: implement sign-in functionality and enhance sign-up error handling

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,27 +1,36 @@
 export default () => {
-    const useSignUp = async (body) => {
-        const supabase = useSupabaseClient();
-        await supabase.auth.signUp(body)
+    const supabase = useSupabaseClient();
 
-        supabase.auth.onAuthStateChange((event, session) => {
-            navigateTo('/profile/listings')
-            // if (event === 'INITIAL_SESSION') {
-            //     // handle initial session
-            // } else if (event === 'SIGNED_IN') {
-            //     // handle sign in event
-            // } else if (event === 'SIGNED_OUT') {
-            //     // handle sign out event
-            // } else if (event === 'PASSWORD_RECOVERY') {
-            //     // handle password recovery event
-            // } else if (event === 'TOKEN_REFRESHED') {
-            //     // handle token refreshed event
-            // } else if (event === 'USER_UPDATED') {
-            //     // handle user updated event
-            // }
-        })
+    const useSignUp = async (body: { email: string; password: string }) => {
+        const { error } = await supabase.auth.signUp({
+            email: body.email,
+            password: body.password,
+            options: {
+                emailRedirectTo: `${useRuntimeConfig().public.domain}/check-your-email`
+            }
+        });
+
+        if (error) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: error.message
+            });
+        }
+    };
+
+    const useSignInWithPassword = async (body: { email: string; password: string }) => {
+        const { error } = await supabase.auth.signInWithPassword(body)
+
+        if (error) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: error.message
+            });
+        }
     }
 
     return {
-        useSignUp
-    }
-}
+        useSignUp,
+        useSignInWithPassword
+    };
+};
